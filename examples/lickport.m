3d14e6b5d4df8a4ef9232460e08adb3b9704eb61
Peter Polidoro <peterpolidoro@gmail.com>
2019-12-06 09:03:12 -0500

    Add matlab script comments

--- examples/lickport.m
+++ examples/lickport.m
@@ -1,14 +1,25 @@
+% create matlab node to communicate with other nodes
 matlab_node = ros2node("/matlab_node");
-lickport_publisher = ros2publisher(matlab_node,"/lickport_joint_target", "sensor_msgs/JointState");
+
+% create publisher to send position and velocity commands to the lickport
+lickport_publisher = ros2publisher(matlab_node, ...
+    "/lickport_joint_target", "sensor_msgs/JointState");
+
+% create message to publish
 lickport_message = ros2message("sensor_msgs/JointState");
+
+% specify motor axis names
 lickport_message.name = {'x', 'y', 'z'};
 
+% first lickport position
 lickport_message.position = [1000, 1000, 1000];
-send(lickport_publisher,lickport_message);
+send(lickport_publisher, lickport_message);
 
+%second lickport position
 lickport_message.position = [100, 200, 300];
-send(lickport_publisher,lickport_message);
+send(lickport_publisher, lickport_message);
 
+% cleanup and close nodes when finished
 clear lickport_publisher
 clear lickport_message
 clear matlab_node
